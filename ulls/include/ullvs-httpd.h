289d747324feca0d1c30637456e1658625599a33
pinjiang <pin.jiang@outlook.com>
2018-09-18 10:33:35 +0800

    Version 0.1, 2018-9-18

--- ulls/include/ullvs-httpd.h
+++
@@ -1,6 +0,0 @@
-#ifndef ULLVS_HTTPD_H
-#define ULLVS_HTTPD_H
-
-void server_callback (SoupServer *server, SoupMessage *msg, const char *path, GHashTable *query, SoupClientContext *context, gpointer data);
-
-#endif
